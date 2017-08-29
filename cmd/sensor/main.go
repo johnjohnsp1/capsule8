707d6be9c99e558d518dc94b65cf6bcba67d1afa
travertischio <traver@capsule8.com>
2017-08-29 10:41:55 -0400

    Adds healthcheck endpoint for sensor and recorder

--- cmd/sensor/main.go
+++ cmd/sensor/main.go
@@ -4,9 +4,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net/http"
 
+	"github.com/capsule8/reactive8/pkg/config"
+	checks "github.com/capsule8/reactive8/pkg/health"
+	"github.com/coreos/pkg/health"
 	"github.com/golang/glog"
 )
+
+var healthChecker health.Checker
 
 func main() {
 	flag.Parse()
@@ -23,4 +30,16 @@ func main() {
 	// Blocking call to remove stale subscriptions on a 5 second interval
 	s.RemoveStaleSubscriptions()
 	close(stopSignal)
+
+	configureHealthChecks()
+	http.HandleFunc("/healthz", healthChecker.ServeHTTP)
+}
+
+// configure and initialize all Checkable variables required by the health checker
+func configureHealthChecks() {
+	stanChecker := checks.ConnectionURL(fmt.Sprintf("%s/streaming/serverz", config.Backplane.NatsMonitoringURL))
+
+	healthChecker.Checks = []health.Checkable{
+		stanChecker,
+	}
 }
