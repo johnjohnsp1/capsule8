68bf9b715c85b5bcdf059a5920bcd383ca455199
Dino A. Dai Zovi <dino@capsule8.com>
2017-12-01 16:50:26 -0500

    Add timeouts to network test script and do TCP/UDP 4 and 6.

--- test/functional/network/network.sh
+++ test/functional/network/network.sh
@@ -2,6 +2,14 @@
 
 PORT=${1:-80}
 
-nc -l ${PORT} &
-sleep 1
-echo 'Hello, World!' | nc localhost ${PORT}
+(sleep 1; echo 'Hello, TCP4!') | timeout 5 nc -4 localhost ${PORT} &
+timeout 2 nc -4l ${PORT} | grep TCP4
+
+(sleep 1; echo 'Hello, TCP6!') | timeout 5 nc -6 localhost ${PORT} &
+timeout 2 nc -6l ${PORT} | grep TCP6
+
+(sleep 1; echo 'Hello, UDP4!') | timeout 5 nc -4u localhost ${PORT} &
+timeout 2 nc -4lu ${PORT} | grep UDP4
+
+(sleep 1; echo 'Hello, UDP6!') | timeout 5 nc -6u localhost ${PORT} &
+timeout 2 nc -6lu ${PORT} | grep UDP6
