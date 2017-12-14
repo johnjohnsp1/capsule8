8806ff27159b9856ef1d33caed2bf26862f7299a
Dino A. Dai Zovi <dino@capsule8.com>
2017-12-14 00:04:15 -0500

    Refactor Kinesis Firehose ingestor a bit.

--- examples/ingestor/main.go
+++ examples/ingestor/main.go
@@ -3,14 +3,12 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"net"
 	"os"
 	"os/signal"
-	"strconv"
 	"strings"
 	"time"
 
@@ -21,23 +19,38 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/firehose"
+	"github.com/aws/aws-sdk-go/service/firehose/firehoseiface"
 	api "github.com/capsule8/capsule8/api/v0"
-	"github.com/capsule8/capsule8/pkg/expression"
 	"github.com/golang/glog"
 	"github.com/golang/protobuf/jsonpb"
-	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes/wrappers"
 )
 
 const (
-	maxBatchSize       = 400
+	// Buffer length of channel for queueing batches of records to send to Kinesis Firehose
+	batchBufferLength = 1024
+
+	// Buffer length of channel for queuing telemetry events to batch up and send to Kinesis
+	channelBufferLength = 1024
+
+	// Maximum number of records permitted in a batch. Amazon's hard limit is 500.
+	maxBatchSize = 500
+
+	// Send the batch after this amount of time if non-empty
+	batchTimerMillis = 1000
+
 	deliveryStreamName = "example-kinesis-stream"
 )
 
-type telemetry struct {
-	PublishTimeMicros string
-	PublishTime       string
-	Event             interface{}
+var hostname string
+
+//
+// This struct is used to define the JSON object that is sent as the
+// record data to Firehose
+//
+type firehoseEvent struct {
+	Hostname       string
+	Timestamp      time.Time
+	TelemetryEvent interface{}
 }
 
 // Custom gRPC Dialer that understands "unix:/path/to/sock" as well as TCP addrs
@@ -69,47 +82,25 @@ func createSubscription() api.Subscription {
 		},
 	}
 
-	syscallEvents := []*api.SyscallEventFilter{
-		// Get all syscalls that return an error
-		&api.SyscallEventFilter{
-			Type: api.SyscallEventType_SYSCALL_EVENT_TYPE_EXIT,
-
-			Id: &wrappers.Int64Value{
-				Value: 2, // SYS_OPEN
-			},
+	networkEvents := []*api.NetworkEventFilter{
+		&api.NetworkEventFilter{
+			Type: api.NetworkEventType_NETWORK_EVENT_TYPE_CONNECT_ATTEMPT,
 		},
-	}
 
-	fileEvents := []*api.FileEventFilter{
-		&api.FileEventFilter{
-			Type: api.FileEventType_FILE_EVENT_TYPE_OPEN,
-
-			//
-			// The glob accepts a wild card character
-			// (*,?) and character classes ([).
-			//
-			FilenamePattern: &wrappers.StringValue{
-				Value: "*foo*",
-			},
+		&api.NetworkEventFilter{
+			Type: api.NetworkEventType_NETWORK_EVENT_TYPE_BIND_ATTEMPT,
 		},
-	}
 
-	sinFamilyFilter := expression.Equal(
-		expression.Identifier("sin_family"),
-		expression.Value(uint16(2)))
-	kernelCallEvents := []*api.KernelFunctionCallFilter{
-		//
-		// Install a kprobe on connect(2)
-		//
-		&api.KernelFunctionCallFilter{
-			Type:   api.KernelFunctionCallEventType_KERNEL_FUNCTION_CALL_EVENT_TYPE_ENTER,
-			Symbol: "SyS_connect",
-			Arguments: map[string]string{
-				"sin_family": "+0(%si):u16",
-				"sin_port":   "+2(%si):u16",
-				"sin_addr":   "+4(%si):u32",
-			},
-			FilterExpression: sinFamilyFilter,
+		&api.NetworkEventFilter{
+			Type: api.NetworkEventType_NETWORK_EVENT_TYPE_BIND_RESULT,
+		},
+
+		&api.NetworkEventFilter{
+			Type: api.NetworkEventType_NETWORK_EVENT_TYPE_ACCEPT_ATTEMPT,
+		},
+
+		&api.NetworkEventFilter{
+			Type: api.NetworkEventType_NETWORK_EVENT_TYPE_ACCEPT_RESULT,
 		},
 	}
 
@@ -128,29 +119,10 @@ func createSubscription() api.Subscription {
 		},
 	}
 
-	// Ticker events are used for debugging and performance testing
-	tickerEvents := []*api.TickerEventFilter{
-		&api.TickerEventFilter{
-			Interval: int64(1 * time.Second),
-		},
-	}
-
-	chargenEvents := []*api.ChargenEventFilter{
-	/*
-		&api.ChargenEventFilter{
-			Length: 16,
-		},
-	*/
-	}
-
 	eventFilter := &api.EventFilter{
-		ProcessEvents:   processEvents,
-		SyscallEvents:   syscallEvents,
-		KernelEvents:    kernelCallEvents,
-		FileEvents:      fileEvents,
 		ContainerEvents: containerEvents,
-		TickerEvents:    tickerEvents,
-		ChargenEvents:   chargenEvents,
+		NetworkEvents:   networkEvents,
+		ProcessEvents:   processEvents,
 	}
 
 	sub := api.Subscription{
@@ -160,16 +132,69 @@ func createSubscription() api.Subscription {
 	return sub
 }
 
+type mockFirehoseClient struct {
+	firehoseiface.FirehoseAPI
+}
+
+func (m *mockFirehoseClient) DescribeDeliveryStream(input *firehose.DescribeDeliveryStreamInput) (*firehose.DescribeDeliveryStreamOutput, error) {
+	output := &firehose.DescribeDeliveryStreamOutput{}
+	return output, nil
+}
+
+func (m *mockFirehoseClient) PutRecordBatch(input *firehose.PutRecordBatchInput) (*firehose.PutRecordBatchOutput, error) {
+	glog.Infof("PutRecordBatch: %d records", len(input.Records))
+	output := &firehose.PutRecordBatchOutput{}
+	return output, nil
+}
+
+func describeDeliveryStream(svc firehoseiface.FirehoseAPI, deliveryStreamName string) (string, error) {
+	ddsOutput, err := svc.DescribeDeliveryStream(&firehose.DescribeDeliveryStreamInput{
+		DeliveryStreamName: aws.String(deliveryStreamName),
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return ddsOutput.String(), nil
+}
+
 func main() {
-	url := flag.String("u", "capsule8-apiserver:8484",
+	url := flag.String("u", "unix:/var/run/capsule8/sensor.sock",
 		"Hostname & port of Sensor gRPC API")
+
 	configFile := flag.String("c", "",
 		"Path to recorder configuration file to use for subscription")
+
+	useMock := flag.Bool("mock", false, "Use mock Firehose API")
 
 	flag.Set("logtostderr", "true")
 	flag.Parse()
 
-	firehoseService := firehose.New(session.New())
+	var err error
+
+	//
+	// Set our hostname (global)
+	//
+	hostname, err = os.Hostname()
+	if err != nil {
+		glog.Fatalf("Couldn't get hostname: %v", err)
+	}
+
+	var firehoseService firehoseiface.FirehoseAPI
+	if *useMock {
+		firehoseService = &mockFirehoseClient{}
+	} else {
+		firehoseService = firehose.New(session.New())
+	}
+
+	// Make sure that we can connect the Kinesis Firehose stream otherwise there isn't anything to do
+	ddsOutput, err := describeDeliveryStream(firehoseService, deliveryStreamName)
+	if err != nil {
+		glog.Fatalf("Couldn't describe delivery stream %s: %v", deliveryStreamName, err)
+	}
+
+	glog.Infof("Delivering to Kinesis Firehose Stream: %v", ddsOutput)
 
 	// Create telemetry service client
 	conn, err := grpc.Dial(*url,
@@ -182,8 +207,6 @@ func main() {
 	}
 
 	c := api.NewTelemetryServiceClient(conn)
-	pc := api.NewPubsubServiceClient(conn)
-
 	ctx, cancel := context.WithCancel(context.Background())
 
 	var sub api.Subscription
@@ -198,20 +221,6 @@ func main() {
 	} else {
 		sub = createSubscription()
 	}
-
-	acksChan := make(chan [][]byte, 128)
-	go func() {
-		for {
-			acks, ok := <-acksChan
-			if ok {
-				pc.Acknowledge(ctx, &api.AcknowledgeRequest{
-					Acks: acks,
-				})
-			} else {
-				return
-			}
-		}
-	}()
 
 	stream, err := c.GetEvents(ctx, &api.GetEventsRequest{
 		Subscription: &sub,
@@ -231,91 +240,145 @@ func main() {
 		cancel()
 	}()
 
-	ticker := time.NewTicker(1 * time.Second)
+	recordsChan := make(chan []*firehose.Record, channelBufferLength)
+	kinesisBatchChan := make(chan []*firehose.Record, batchBufferLength)
 
-	msgs := 0
-	bytes := 0
-
+	//
+	// Create a goroutine to pull from queue of batches of events to send to Kinesis Firehose
+	//
 	go func() {
 		for {
 			select {
-			case <-ticker.C:
-				fmt.Printf("%d msgs/s %d KB/s\n", msgs, bytes/1024)
-				msgs = 0
-				bytes = 0
+			case records, ok := <-kinesisBatchChan:
+				if ok {
+					output, err := firehoseService.PutRecordBatch(&firehose.PutRecordBatchInput{
+						DeliveryStreamName: aws.String(deliveryStreamName),
+						Records:            records,
+					})
+
+					if err != nil {
+						glog.Fatal(err)
+					}
+
+					glog.V(1).Infof("PutRecordBatch: %s", output)
+				} else {
+					// kinesisBatchChan has been closed, so we should exit the goroutine
+					glog.Info("Channel closed, exiting Kinesis Firehose sender goroutine")
+					return
+				}
 			}
 		}
 	}()
 
-	for {
-		response, err := stream.Recv()
-		if err != nil {
-			glog.Fatalf("Recv: %s\n", err)
-		}
-
-		acks := [][]byte{}
-		for _, event := range response.Events {
-			glog.V(1).Infof("%+v", event)
-
-			err := putTelemetryRecordKinesis(event)
+	//
+	// Create a goroutine to receive events from the gRPC stream,
+	// convert them to Firehose Records, and push them onto a
+	// channel for batching up to send to a delivery stream.
+	//
+	go func() {
+		for {
+			response, err := stream.Recv()
 			if err != nil {
-				glog.Errorf("failed to push telemetry to kinesis: %s", err)
+				glog.Errorf("Error received from gRPC stream: %v", err)
+
+				// Close the eventChan and return
+				// since there won't be any more
+				// events sent to it
+				close(recordsChan)
+
+				return
 			}
 
-			msgs++
-			b, _ := proto.Marshal(event)
-			bytes += len(b)
-
-			if len(event.Ack) > 0 {
-				acks = append(acks, event.Ack)
+			kinesisRecords, err := convertTelemetryToFirehoseRecords(response.Events)
+			if err != nil {
+				glog.Fatal(err)
 			}
+
+			recordsChan <- kinesisRecords
 		}
+	}()
 
-		if len(acks) > 0 {
-			acksChan <- acks
+	//
+	// Batching for Kinesis: PutRecord accepts up to 500 records
+	//
+	var kinesisBatch []*firehose.Record
+
+	timerDuration := time.Duration(batchTimerMillis * time.Millisecond)
+	timer := time.NewTimer(timerDuration)
+
+	for {
+		select {
+		case records, ok := <-recordsChan:
+			if ok {
+				kinesisBatch = append(kinesisBatch, records...)
+
+				if len(kinesisBatch) >= maxBatchSize {
+					kinesisBatchChan <- kinesisBatch[:maxBatchSize]
+					kinesisBatch = kinesisBatch[maxBatchSize:]
+
+					// Reset timer
+					timer.Reset(timerDuration)
+				}
+			} else {
+				// eventsChan has been closed: send
+				// the last batch, close the firehose
+				// batch channel, and exit goroutine
+				kinesisBatchChan <- kinesisBatch
+				close(kinesisBatchChan)
+				return
+			}
+
+		case <-timer.C:
+			// Timer fired, queue up the batch of the
+			// events that we have for sending
+			if len(kinesisBatch) > 0 {
+				kinesisBatchChan <- kinesisBatch
+				kinesisBatch = []*firehose.Record{}
+			}
+
+			timer.Reset(timerDuration)
 		}
 	}
 }
 
-func putTelemetryRecordKinesis(telemetryEvent *api.TelemetryEvent) error {
-	var event telemetry
-	var eventData bytes.Buffer
+//
+// Convert received telemetry event from proto to JSON and enrich with a
+// few fields.
+//
+func convertTelemetryToFirehoseRecords(telemetryEvents []*api.TelemetryEvent) ([]*firehose.Record, error) {
+	var records []*firehose.Record
 
 	jsonMarshaller := jsonpb.Marshaler{
 		EmitDefaults: true,
 	}
 
-	err := jsonMarshaller.Marshal(&eventData, telemetryEvent)
-	if err != nil {
-		return err
+	for _, telemetryEvent := range telemetryEvents {
+		jsonString, err := jsonMarshaller.MarshalToString(telemetryEvent)
+		if err != nil {
+			glog.Warning(err)
+			return records, nil
+		}
+
+		// Unmarshal to a map to allow us to modify it
+		var d map[string]interface{}
+		json.Unmarshal([]byte(jsonString), &d)
+
+		delete(d, "publishTimeMicros")
+		delete(d, "ack")
+
+		d["hostname"] = hostname
+		d["timestamp"] = time.Now()
+
+		jsonBytes, err := json.Marshal(d)
+		if err != nil {
+			glog.Warning(err)
+			return records, nil
+		}
+
+		glog.V(2).Infof("%s", string(jsonBytes))
+		r := &firehose.Record{Data: jsonBytes}
+		records = append(records, r)
 	}
 
-	err = json.Unmarshal(eventData.Bytes(), &event)
-	if err != nil {
-		return err
-	}
-
-	publishTimeInt, err := strconv.ParseInt(event.PublishTimeMicros, 10, 64)
-	if err != nil {
-		return err
-	}
-
-	publishTime := time.Unix(0, publishTimeInt)
-	event.PublishTime = publishTime.Format(time.RFC3339)
-
-	eb, err := json.Marshal(event)
-
-	firehoseService := firehose.New(session.New())
-	output, err := firehoseService.PutRecord(&firehose.PutRecordInput{
-		DeliveryStreamName: aws.String(deliveryStreamName),
-		Record: &firehose.Record{
-			Data: eb,
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	glog.V(1).Infof("Record: %v\n Put Record Result: %+v\n\n", string(eb), output)
-	return nil
+	return records, nil
 }
